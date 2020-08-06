622279712fdac05c186edbc4ca1a55651fab24c1
Shun_Harutaro <n.shunshun2004@gmail.com>
2020-08-06 22:01:27 +0900

    GCPに対応させた

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import flask
 import PyPDF2
 import linecache
+import urllib.request
+import os
 
-FILE_PATH = 'http://math.dge.toyota-ct.ac.jp/katsutani/lssn/2020/ba2/wkprntans/f2i/2i30ans18.pdf'
+def correct(request):
+    url = 'http://math.dge.toyota-ct.ac.jp/katsutani/lssn/2020/ba2/wkprntans/f2i/2i30ans18.pdf'
+    savename = "/tmp/print.pdf"
+    urllib.request.urlretrieve(url, savename)
 
-with open(FILE_PATH, "rb") as f:
-    reader = PyPDF2.PdfFileReader(f)
-    page = reader.getPage(0)
-    text = page.extractText()
-
-line = text.splitlines()
-print(text)
-print("あなたの18回目時点での正答数は"+line[7]+"問です。")
+    with open(savename, "rb") as f:
+        reader = PyPDF2.PdfFileReader(f)
+        page = reader.getPage(0)
+        text = page.extractText()
+    line = text.splitlines()
+    result = line[7]
+    os.remove('/tmp/print.pdf')
+    return flask.make_response(flask.jsonify(result))
